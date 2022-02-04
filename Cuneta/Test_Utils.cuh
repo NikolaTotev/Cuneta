5edc8222847c361b70b496ab109b54bdee746fed
Nikola <nikola.r.totev@gmail.com>
2022-02-04 16:06:41 +0200

    Update Test_Utils.cuh

--- Cuneta/Test_Utils.cuh
+++ Cuneta/Test_Utils.cuh
@@ -1,5 +1,5 @@
 #include "ReLU.cuh"
 
 void TestReLU(float* inputMatrix, float* outputMatrix, int matrixWidth, int matrixHeight);
-void TestMaxPool(float* inputMatrix, float* outputMatrix, int matrixWidth, int matrixHeight);
-void TestConvolution(float* inputMatrix, int matrixHeight, int matrixWidth, int filterSize);
+void TestMaxPool(float* inputMatrix, float* outputMatrix, int matrixWidth, int matrixHeight, bool printMatricies);
+void TestConvolution(float* inputMatrix, int matrixHeight, int matrixWidth, int filterSize, bool printMatricies);
