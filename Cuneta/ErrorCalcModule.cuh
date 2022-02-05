6379b8ea9445a833d300d8e8270323fd67279743
Nikola <nikola.r.totev@gmail.com>
2022-02-05 02:04:54 +0200

    Create ErrorCalcModule.cuh

---
+++ Cuneta/ErrorCalcModule.cuh
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef GRADIENT_GPU_H
+#define GRADIENT_GPU_H
+
+#include <crt/host_defines.h>
+
+#include "CunetaModule.cuh"
+
+
+class ErrorCalcModule : public CunetaModule
+{
+public:
+	ErrorCalcModule(float* _inputMatrix, float*_groundTruth,int _inputHeight, int _inputWidth);
+	float networkError;
+	float* groundTruthMatrix;
+	float* sigmoidResultMatrix;
+	float* crossEntropyResultMatrix;
+	void ForwardPass() override;
+	void BackwardPass() override;
+	void UpdateModule() override;
+	void PixelWiseSoftMax();
+	void PixelWiseCrossEntropy();
+	void CrossEntropySum();
+	
+};
+
+#endif
