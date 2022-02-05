02caa48081643e022193389190b067b8c77c43dc
Nikola <nikola.r.totev@gmail.com>
2022-02-05 14:44:33 +0200

    Added test for image ingester

--- Cuneta/Test_Utils.cuh
+++ Cuneta/Test_Utils.cuh
@@ -1,7 +1,11 @@
+#include <string>
+
 #include "ReLU.cuh"
+using namespace std;
 
 void TestReLU(float* inputMatrix, float* outputMatrix, int matrixWidth, int matrixHeight);
 void TestMaxPool(float* inputMatrix, float* outputMatrix, int matrixWidth, int matrixHeight, bool printMatricies);
 void TestConvolution(float* inputMatrix, int matrixHeight, int matrixWidth, int filterSize, bool printMatricies);
 void TestTransposeConvolution(float* inputMatrix, int matrixHeight, int matrixWidth, int filterSize, bool printMatricies);
-void TestErrorCalcModule(float* inputMatrix, float * groundTruthMatrix, int matrixHeight, int matrixWidth, bool printMatricies);
+void TestErrorCalcModule(float* inputMatrix, float* groundTruthMatrix, int matrixHeight, int matrixWidth, bool printMatricies);
+void TestImageIngester(string inputPath, string groundTruthPath, bool printMatricies);
