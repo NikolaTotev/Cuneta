35c19a699e24e99b1369514529660b15eb46bb9c
Nikola <nikola.r.totev@gmail.com>
2022-02-11 13:42:55 +0200

    Completed all core Cuneta modules

--- Cuneta/Convolution.cuh
+++ Cuneta/Convolution.cuh
@@ -43,13 +43,20 @@ public:
 	float** L_AdamOptimizer_Corrected_V_Matrix;
 	float** L_AdamOptimizer_Corrected_S_Matrix;
 
+	float** L_Biases;
+	float** L_BIAS_AdamOptimizer_V_Matrix;
+	float** L_BIAS_AdamOptimizer_S_Matrix;
+	float** L_BIAS_AdamOptimizer_Corrected_V_Matrix;
+	float** L_BIAS_AdamOptimizer_Corrected_S_Matrix;
+
+
 	void ForwardPass(float* forwardPassInput, int fwdPassHeight, int fwdPassWidth) override;
 	void BackwardPass(float* backpropInput, int backPassHeight, int backPassWidth) override;
 	void FilterBackprop(float* backpropInput, int backPassHeight, int backPassWidth);
 	void LayerForwardPass(float** _inputs) override;
 	void LayerBackwardPass(float** _backpropInput) override;
 	void LayerFilterBackprop();
-	void LayerBiasBackprop();
+	void LayerBiasUpdate();
 	void LayerFilterInitialization();
 	void LayerFlipFilter();
 	void LayerPadBackpropInput();
