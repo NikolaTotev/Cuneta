a1b38ddf1fbf2884fd17b65d96e138a2cf2d0db0
Nikola <nikola.r.totev@gmail.com>
2022-02-12 02:43:50 +0200

    Update MaxPool.cuh

--- Cuneta/MaxPool.cuh
+++ Cuneta/MaxPool.cuh
@@ -11,12 +11,14 @@
 class MaxPool : public CunetaModule
 {
 public:
-	MaxPool(int _numberOfInputs, int _numberOfOutputs, int _inputHeight, int _inputWidth);
+	MaxPool(int _numberOfInputs, int _numberOfOutputs, int _inputHeight, int _inputWidth, int _layerID, int _levelID);
 	void ForwardPass(float* forwardPassInput, int fwdPassHeight, int fwdPassWidth) override;
 	void BackwardPass(float* backpropInput, int backPassHeight, int backPassWidth) override;
 	void LayerForwardPass(float** _inputs) override;
 	void LayerBackwardPass(float** _backpropInput) override;
 	void UpdateModule() override;
+	void Print();
+	void PrintLayerParams();
 
 	//Input and output will be in global memory. d_ shows in which memory the variables are stored.
 };
