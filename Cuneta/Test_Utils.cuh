03f24826d102492cad69b5e7978cbb76d70398c5
Nikola <nikola.r.totev@gmail.com>
2022-02-13 02:35:13 +0200

    Update Test_Utils.cuh

--- Cuneta/Test_Utils.cuh
+++ Cuneta/Test_Utils.cuh
@@ -1,20 +1,25 @@
-//#include <string>
-//
-//#include "ReLU.cuh"
-//using namespace std;
-//
-//void TestReLU(int inputMatrixWidth, int inputMatrixHeight, int minInputVal, int maxInputVal, bool printMatricies);
-//void TestBackpropReLU(int inputMatrixWidth, int inputMatrixHeight, int fwdMinInputVal, int fwdMaxInputVal, int backMinInputVal, int backMaxInputVal, bool printMatricies);
-//
-//void TestMaxPool(int inputMatrixWidth, int inputMatrixHeight, int minInputVal, int maxInputVal, bool printMatricies);
-//void TestBackpropMaxPool(int inputMatrixWidth, int inputMatrixHeight, int fwdMinInputVal, int fwdMaxInputVal, int backMinInputVal, int backMaxInputVal, bool printMatricies);
-//
-//void TestConvolution(int inputMatrixWidth, int inputMatrixHeight, int minInputVal, int maxInputVal, int filterSize, int paddingSize, bool printMatricies);
-//void TestBackpropConvolution(int inputMatrixWidth, int inputMatrixHeight, int fwdMinInputVal, int fwdMaxInputVal, int backMinInputVal, int backMaxInputVal, int filterSize, int paddingSize, bool printMatricies);
-//
-//void TestTransposeConvolution(int inputMatrixWidth, int inputMatrixHeight, int minInputVal, int maxInputVal, int filterSize, int paddingSize, bool printMatricies);
-//void TestBackpropTransposeConvolution(int inputMatrixWidth, int inputMatrixHeight, int fwdMinInputVal, int fwdMaxInputVal, int backMinInputVal, int backMaxInputVal, int filterSize, int paddingSize, bool printMatricies);
-//
-//void TestBackpropErrorCalcModule(int inputMatrixWidth, int inputMatrixHeight, int rawMinInput, int rawMaxInput, int groundTruthMinInput, int groundTruthMaxInput, bool printMatricies);
-//
-//void TestImageIngester(string inputPath, string groundTruthPath, bool printMatricies);
+#include <string>
+#include "Convolution.cuh"
+#include "ErrorCalcModule.cuh"
+#include "ImageIngester.cuh"
+#include "Logger.cuh"
+#include "MaxPool.cuh"
+#include "ReLU.cuh"
+#include "TransposeConvolution.cuh"
+
+using namespace std;
+class NetworkValidator
+{
+	string testSaveDirectory = "";
+
+public:
+	NetworkValidator(string _saveDirectory);
+	void TestFlowController();
+	void TestReLU();
+	void TestMaxPool();
+	void TestConvolution();
+	void TestTransposeConvolution();
+	void SquishTest();
+	void TestSumBlock();
+	void TestErrorBlock();
+};
