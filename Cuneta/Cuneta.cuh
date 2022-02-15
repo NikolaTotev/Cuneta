c50d6b948617a3a6f77e60afc3fdd293f36de2b0
Nikola <nikola.r.totev@gmail.com>
2022-02-16 01:52:31 +0200

    Update Cuneta.cuh

--- Cuneta/Cuneta.cuh
+++ Cuneta/Cuneta.cuh
@@ -3,12 +3,14 @@
 #include "ImageIngester.cuh"
 #include "MaxPool.cuh"
 #include "ReLU.cuh"
+#include "Squishy.cuh"
 #include "SumBlock.cuh"
 #include "TransposeConvolution.cuh"
 
 
 class Cuneta
 {
+public:
 	Convolution Convolution_Level_1_Layer_1;
 	ReLU ReLU_Level_1_Layer_1;
 	Convolution Convolution_Level_1_Layer_2;
@@ -43,46 +45,43 @@ class Cuneta
 	Convolution Convolution_Level_5_Layer_2;
 	ReLU ReLU_Level_5_Layer_2;
 
-	Convolution SkipConnection_1;
+	Convolution SkipConnection_Convolution_1;
 	TransposeConvolution TransposeConvolution_1;
 	SumBlock SumBlock_1;
 	
 	Convolution Convolution_Level_6_Layer_1;
 	ReLU ReLU_Level_6_Layer_1;
-	Convolution Convolution_Level_6_Layer_2;
-	ReLU ReLU_Level_6_Layer_2;
 
-	Convolution SkipConnection_2;
+	Convolution SkipConnection_Convolution_2;
 	TransposeConvolution TransposeConvolution_2;
 	SumBlock SumBlock_2;
 
 	Convolution Convolution_Level_7_Layer_1;
 	ReLU ReLU_Level_7_Layer_1;
-	Convolution Convolution_Level_7_Layer_2;
-	ReLU ReLU_Level_7_Layer_2;
 
-	Convolution SkipConnection_3;
+	Convolution SkipConnection_Convolution_3;
 	TransposeConvolution TransposeConvolution_3;
 	SumBlock SumBlock_3;
 
 	Convolution Convolution_Level_8_Layer_1;
 	ReLU ReLU_Level_8_Layer_1;
-	Convolution Convolution_Level_8_Layer_2;
-	ReLU ReLU_Level_8_Layer_2;
-
-	Convolution SkipConnection_4;
+	
+	Convolution SkipConnection_Convolution_4;
 	TransposeConvolution TransposeConvolution_4;
 	SumBlock SumBlock_4;
 
 	Convolution Convolution_Level_9_Layer_1;
 	ReLU ReLU_Level_9_Layer_1;
-	Convolution Convolution_Level_9_Layer_2;
-	ReLU ReLU_Level_9_Layer_2;
+
+	Squishy Squoosh_1;
+
+	//ErrorCalcModule Err_Module;
 
 	int Input_HEIGHT;
 	int Input_WIDTH;
 	float** Input;
 	Cuneta(int _height, int _width, float** _input);
+	void PrintSetup();
 	void Train(string dataDirectory, string logDirectory);
 	void Segment(string inputImage, string logDirectory);
 	void Save(string savePath);
